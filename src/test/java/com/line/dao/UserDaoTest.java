f2dd13506b16a18c9e150a9658adbfc6fb4f06dc
soonmineom <skdlfma123@gmail.com>
2022-10-20 13:32:25 +0900

    deleteAll()을 사용해 test 기능을 보완

--- src/test/java/com/line/dao/UserDaoTest.java
+++ src/test/java/com/line/dao/UserDaoTest.java
@@ -25,16 +25,38 @@ class UserDaoTest {
     void addAndSelect() throws SQLException, ClassNotFoundException {
 
         UserDao userDao = context.getBean("aUserDao", UserDao.class);
-       // User user = new User("1", "soonmin","1234");
-       // userDao.add(user);
+        userDao.deleteAll();
+
+        User user = new User("1", "soonmin","1234");
+        userDao.add(user);
+        assertEquals(1,userDao.getCount());
 
         User selecteduser = userDao.select("1");
         assertEquals("soonmin", selecteduser.getName());
+
+        userDao.deleteAll();
+        assertEquals(0,userDao.getCount());
     }
 
     @Test
-    void deleteAll() throws SQLException, ClassNotFoundException {
+    void count() throws SQLException, ClassNotFoundException {
         UserDao userDao = context.getBean("aUserDao", UserDao.class);
+
+        User user1 = new User("1","a","aaaa");
+        User user2 = new User("2","b","bbbb");
+        User user3 = new User("3","c","cccc");
+
         userDao.deleteAll();
+        assertEquals(0,userDao.getCount());
+
+        userDao.add(user1);
+        assertEquals(1,userDao.getCount());
+
+        userDao.add(user2);
+        assertEquals(2,userDao.getCount());
+
+        userDao.add(user3);
+        assertEquals(3,userDao.getCount());
+
     }
 }
